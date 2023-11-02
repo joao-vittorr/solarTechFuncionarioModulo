9a5827489eb7112e4b444be526414e5d0b84640b
João Vitor <vitorribeiro9454@gmail.com>
2023-11-02 11:36:12 -0300

    corrigindo tabela e removendo buscar do sidebar

--- resources/views/vendas.blade.php
+++ resources/views/vendas.blade.php
@@ -27,7 +27,7 @@
 -->
 
 <body class="hold-transition sidebar-mini">
-<div class="wrapper">
+  <div class="wrapper">
     <!-- Navbar -->
     <nav class="main-header navbar navbar-expand navbar-white navbar-light">
       <!-- Left navbar links -->
@@ -71,115 +71,115 @@
                   </button>
             </form>
           </div>
-        </div>
-        </form>
-        </div>
-        </li>
+  </div>
+  </form>
+  </div>
+  </li>
 
-        <!-- Messages Dropdown Menu -->
-        <li class="nav-item dropdown">
-          <a class="nav-link" data-toggle="dropdown" href="#">
-            <i class="far fa-comments"></i>
-            <span class="badge badge-danger navbar-badge">3</span>
-          </a>
-          <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-            <a href="#" class="dropdown-item">
-              <!-- Message Start -->
-              <div class="media">
-                <img src="" alt="User Avatar" class="img-size-50 mr-3 img-circle">
-                <div class="media-body">
-                  <h3 class="dropdown-item-title">
-                    Brad Diesel
-                    <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
-                  </h3>
-                  <p class="text-sm">Call me whenever you can...</p>
-                  <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                </div>
-              </div>
-              <!-- Message End -->
-            </a>
-            <div class="dropdown-divider"></div>
-            <a href="#" class="dropdown-item">
-              <!-- Message Start -->
-              <div class="media">
-                <img src="dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
-                <div class="media-body">
-                  <h3 class="dropdown-item-title">
-                    John Pierce
-                    <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
-                  </h3>
-                  <p class="text-sm">I got your message bro</p>
-                  <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                </div>
-              </div>
-              <!-- Message End -->
-            </a>
-            <div class="dropdown-divider"></div>
-            <a href="#" class="dropdown-item">
-              <!-- Message Start -->
-              <div class="media">
-                <img src="dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
-                <div class="media-body">
-                  <h3 class="dropdown-item-title">
-                    Nora Silvester
-                    <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
-                  </h3>
-                  <p class="text-sm">The subject goes here</p>
-                  <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                </div>
-              </div>
-              <!-- Message End -->
-            </a>
-            <div class="dropdown-divider"></div>
-            <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
+  <!-- Messages Dropdown Menu -->
+  <li class="nav-item dropdown">
+    <a class="nav-link" data-toggle="dropdown" href="#">
+      <i class="far fa-comments"></i>
+      <span class="badge badge-danger navbar-badge">3</span>
+    </a>
+    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+      <a href="#" class="dropdown-item">
+        <!-- Message Start -->
+        <div class="media">
+          <img src="" alt="User Avatar" class="img-size-50 mr-3 img-circle">
+          <div class="media-body">
+            <h3 class="dropdown-item-title">
+              Brad Diesel
+              <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
+            </h3>
+            <p class="text-sm">Call me whenever you can...</p>
+            <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
           </div>
-        </li>
-        <!-- Notifications Dropdown Menu -->
-        <li class="nav-item dropdown">
-          <a class="nav-link" data-toggle="dropdown" href="#">
-            <i class="far fa-bell"></i>
-            <span class="badge badge-warning navbar-badge">15</span>
-          </a>
-          <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-            <span class="dropdown-item dropdown-header">15 Notifications</span>
-            <div class="dropdown-divider"></div>
-            <a href="#" class="dropdown-item">
-              <i class="fas fa-envelope mr-2"></i> 4 new messages
-              <span class="float-right text-muted text-sm">3 mins</span>
-            </a>
-            <div class="dropdown-divider"></div>
-            <a href="#" class="dropdown-item">
-              <i class="fas fa-users mr-2"></i> 8 friend requests
-              <span class="float-right text-muted text-sm">12 hours</span>
-            </a>
-            <div class="dropdown-divider"></div>
-            <a href="#" class="dropdown-item">
-              <i class="fas fa-file mr-2"></i> 3 new reports
-              <span class="float-right text-muted text-sm">2 days</span>
-            </a>
-            <div class="dropdown-divider"></div>
-            <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
+        </div>
+        <!-- Message End -->
+      </a>
+      <div class="dropdown-divider"></div>
+      <a href="#" class="dropdown-item">
+        <!-- Message Start -->
+        <div class="media">
+          <img src="dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+          <div class="media-body">
+            <h3 class="dropdown-item-title">
+              John Pierce
+              <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
+            </h3>
+            <p class="text-sm">I got your message bro</p>
+            <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
           </div>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-            <i class="fas fa-expand-arrows-alt"></i>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-            <i class="fas fa-th-large"></i>
-          </a>
-        </li>
-        </ul>
-    </nav>
+        </div>
+        <!-- Message End -->
+      </a>
+      <div class="dropdown-divider"></div>
+      <a href="#" class="dropdown-item">
+        <!-- Message Start -->
+        <div class="media">
+          <img src="dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+          <div class="media-body">
+            <h3 class="dropdown-item-title">
+              Nora Silvester
+              <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
+            </h3>
+            <p class="text-sm">The subject goes here</p>
+            <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+          </div>
+        </div>
+        <!-- Message End -->
+      </a>
+      <div class="dropdown-divider"></div>
+      <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
+    </div>
+  </li>
+  <!-- Notifications Dropdown Menu -->
+  <li class="nav-item dropdown">
+    <a class="nav-link" data-toggle="dropdown" href="#">
+      <i class="far fa-bell"></i>
+      <span class="badge badge-warning navbar-badge">15</span>
+    </a>
+    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+      <span class="dropdown-item dropdown-header">15 Notifications</span>
+      <div class="dropdown-divider"></div>
+      <a href="#" class="dropdown-item">
+        <i class="fas fa-envelope mr-2"></i> 4 new messages
+        <span class="float-right text-muted text-sm">3 mins</span>
+      </a>
+      <div class="dropdown-divider"></div>
+      <a href="#" class="dropdown-item">
+        <i class="fas fa-users mr-2"></i> 8 friend requests
+        <span class="float-right text-muted text-sm">12 hours</span>
+      </a>
+      <div class="dropdown-divider"></div>
+      <a href="#" class="dropdown-item">
+        <i class="fas fa-file mr-2"></i> 3 new reports
+        <span class="float-right text-muted text-sm">2 days</span>
+      </a>
+      <div class="dropdown-divider"></div>
+      <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
+    </div>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+      <i class="fas fa-expand-arrows-alt"></i>
+    </a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
+      <i class="fas fa-th-large"></i>
+    </a>
+  </li>
+  </ul>
+  </nav>
   <!-- /.navbar -->
 
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ route('dashboard') }}" class="brand-link">
-      <img src="{{asset('images/logoProjetoSolarTech.svg')}}" alt="Logo Projeto SolarTech" class="brand-image" style="opacity: .8"/>
+      <img src="{{asset('images/logoProjetoSolarTech.svg')}}" alt="Logo Projeto SolarTech" class="brand-image" style="opacity: .8" />
       <span class="brand-text font-weight-light">SolarTech</span>
     </a>
 
@@ -194,19 +194,6 @@
           <a href="#" class="d-block">Alexander Pierce</a>
         </div>
       </div>
-
-      <!-- SidebarSearch Form -->
-      <div class="form-inline">
-        <div class="input-group" data-widget="sidebar-search">
-          <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-          <div class="input-group-append">
-            <button class="btn btn-sidebar">
-              <i class="fas fa-search fa-fw"></i>
-            </button>
-          </div>
-        </div>
-      </div>
-
       <!-- Sidebar Menu -->
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
@@ -443,81 +430,44 @@
     </div>
     <!-- /.sidebar -->
   </aside>
-
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">Produtos vendidos</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Home</a></li>
-              <li class="breadcrumb-item active">Produtos vendidos</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-
+    <br><br>
+    <h1 class="justify-content-center">Produtos vendidos</h1>
     <!-- Main content -->
-    <div class="content">
+    <div>
       <div class="container-fluid">
         <div class="row">
           <div class="col-lg-12">
-            <!-- /.card -->
-            <div class="card">
-              <div class="card-header border-0">
-                <h3 class="card-title">Products</h3>
-                <div class="card-tools">
-                  <a href="#" class="btn btn-tool btn-sm">
-                    <i class="fas fa-download"></i>
-                  </a>
-                  <a href="#" class="btn btn-tool btn-sm">
-                    <i class="fas fa-bars"></i>
-                  </a>
-                </div>
-              </div>
-              <div class="card-body table-responsive p-0">
-              <table class="table table-striped table-valign-middle">
-                    <thead>
-                        <tr>
-                            <th>Nome do Usuário</th>
-                            <th>Email</th>
-                            <th>Endereço</th>
-                            <th>Nome do Pacote</th>
-                            <th>Quantidade de Placas</th>
-                            <th>Valor Final</th>
-                            <th>Ações</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($dadosVendas as $venda)
-                        <tr>
-                            <td>{{ $venda->user->name }}</td>
-                            <td>{{ $venda->user->email }}</td>
-                            <td>{{ $venda->user->address }}</td>
-                            <td>{{ $venda->nomePacote }}</td>
-                            <td>{{ $venda->quantidadePlacas }}</td>
-                            <td>R$ {{ $venda->valorFinal }}</td>
-                            <td>
-                                <form method="POST" action="{{ route('venda.deletar', ['id' => $venda->id]) }}">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit">Deletar</button>
-                                </form>
-                            </td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-              </div>
-            </div>
-            <!-- /.card -->
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">Usuário</th>
+                  <th scope="col">Pacote</th>
+                  <th scope="col">Qtd de Placas</th>
+                  <th scope="col">Valor Final</th>
+                  <th scope="col">Ações</th>
+                </tr>
+              </thead>
+              <tbody>
+                @foreach($dadosVendas as $venda)
+                <tr>
+                  <td>{{ $venda->user->name }}</td>
+                  <td>{{ $venda->nomePacote }}</td>
+                  <td>{{ $venda->quantidadePlacas }}</td>
+                  <td>R$ {{ $venda->valorFinal }}</td>
+                  <td>
+                    <form method="POST" action="{{ route('venda.deletar', ['id' => $venda->id]) }}">
+                      @csrf
+                      @method('DELETE')
+                      <button type="submit">Deletar</button>
+                    </form>
+                  </td>
+                </tr>
+                @endforeach
+
+              </tbody>
+            </table>
           </div>
         </div>
         <!-- /.row -->
@@ -525,13 +475,13 @@
       <!-- /.container-fluid -->
     </div>
     <!-- /.content -->
-</div>
+  </div>
   <!-- /.content-wrapper -->
 
   <!-- Control Sidebar -->
-<aside class="control-sidebar control-sidebar-dark">
+  <aside class="control-sidebar control-sidebar-dark">
     <!-- Control sidebar content goes here -->
-</aside>
+  </aside>
   <!-- /.control-sidebar -->
 
   <!-- Main Footer -->
