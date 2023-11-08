f3d324b1292d579b73e70f56fd76394b2e702aea
João Vitor <vitorribeiro9454@gmail.com>
2023-11-07 21:36:38 -0300

    mudando template.blade e tabela de usuários

--- resources/views/funcionario.blade.php
+++ resources/views/funcionario.blade.php
@@ -1,67 +1,51 @@
-@extends('layouts.template') 
+@extends('layouts.template')
 
 @section('content')
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <br><br>
-    <h1 class="justify-content-center">Usuários do sistema</h1>
-    <!-- Main content -->
-    <div>
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-lg-12">
-            <!-- Adicione um formulário de busca pelo CPF acima da tabela -->
-            <form method="GET" action="{{ route('gerenciar.funcionario.searchByCPF') }}">
-              <input type="text" name="cpf" placeholder="Buscar por CPF">
-              <button type="submit">Buscar</button>
-            </form>
-
-            <table class="table">
-              <!-- Restante da tabela permanece igual -->
-              <thead>
-                <tr>
-                  <th scope="col">ID</th>
-                  <th scope="col">Nome</th>
-                  <th scope="col">CPF</th>
-                  <th scope="col">Nível Atual</th>
-                  <th scope="col">Atualizar Nível</th>
-                </tr>
-              </thead>
-              <tbody>
-                @foreach($users as $user)
-                <tr>
-                  <td>{{ $user->id }}</td>
-                  <td>{{ $user->name }}</td>
-                  <td>{{ $user->cpf }}</td>
-                  <td>{{ $user->access_level }}</td>
-                  <td>
-                    <form method="POST" action="{{ route('gerenciar.funcionario.updateLevel', $user->id) }}">
-                      @csrf
-                      @method('PUT')
-                      <select name="access_level">
-                        <option value="normal" {{ $user->access_level === 'normal' ? 'selected' : '' }}>Cliente</option>
-                        <option value="funcionario" {{ $user->access_level === 'funcionario' ? 'selected' : '' }}>Funcionário</option>
-                        <option value="admin" {{ $user->access_level === 'admin' ? 'selected' : '' }}>Master</option>
-                      </select>
-                      <button type="submit">Salvar</button>
-                    </form>
-                  </td>
-                  <td>
-                    <!-- Adicione outras ações conforme necessário -->
-                  </td>
-                </tr>
-                @endforeach
-              </tbody>
-            </table>
-
-          </div>
-        </div>
-        <!-- /.row -->
-      </div>
-      <!-- /.container-fluid -->
+<br><br>
+<div>
+  <h1>Usuários do sistema</h1>
+  <!-- Adicione um formulário de busca pelo CPF acima da tabela -->
+  <form method="GET" action="{{ route('gerenciar.funcionario.searchByCPF') }}" class="mb-3">
+    <div class="input-group">
+      <input type="text" name="cpf" placeholder="Buscar por CPF" class="form-control">
+      <button type="submit" class="btn btn-primary">Buscar</button>
     </div>
-    <!-- /.content -->
+  </form>
+  <div class="table-responsive">
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Nome</th>
+          <th>CPF</th>
+          <th>Nível Atual</th>
+          <th>Atualizar Nível</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach($users as $user)
+        <tr>
+          <td>{{ $user->id }}</td>
+          <td>{{ $user->name }}</td>
+          <td>{{ $user->cpf }}</td>
+          <td>{{ $user->access_level }}</td>
+          <td>
+            <form id="form-{{ $user->id }}" method="POST" action="{{ route('gerenciar.funcionario.updateLevel', $user->id) }}">
+              @csrf
+              @method('PUT')
+              <div class="input-group">
+                <select name="access_level" class="form-select access-level" onchange="document.getElementById('form-{{ $user->id }}').submit()">
+                  <option value="normal" {{ $user->access_level === 'normal' ? 'selected' : '' }}>Cliente</option>
+                  <option value="funcionario" {{ $user->access_level === 'funcionario' ? 'selected' : '' }}>Funcionário</option>
+                  <option value="admin" {{ $user->access_level === 'admin' ? 'selected' : '' }}>Master</option>
+                </select>
+              </div>
+            </form>
+          </td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
   </div>
-  <!-- /.content-wrapper -->
+</div>
 @endsection
- 
--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="main-footer">
+<footer class="bg-dark">
     <strong>Copyright &copy; 2023 SolarTech.</strong>
     All rights reserved.
 </footer>
