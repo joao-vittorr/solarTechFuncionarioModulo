04aa2d25708eab2c3d43c82725f9828b51c5b68c
João Vitor <vitorribeiro9454@gmail.com>
2023-11-10 12:30:41 -0300

    Fazendo layout do form despesa

--- resources/views/despesas/form.blade.php
+++ resources/views/despesas/form.blade.php
@@ -1,68 +1,65 @@
 @extends('layouts.template')
 
 @section('content')
-    @if ($despesa && $despesa->id)
-        <h1>Editar Despesa</h1>
-        <form id="main" method="POST" action="{{ route('despesas.update', ['id' => $despesa->id]) }}"
-            enctype="multipart/form-data">
-            @method('PUT')
+        @if ($despesa && $despesa->id)
+            <div class="container">
+            <h1>Editar Despesa</h1>
+            <form id="main" method="POST" action="{{ route('despesas.update', ['id' => $despesa->id]) }}" enctype="multipart/form-data">
+        @method('PUT')
         @else
+        <div class="container">
             <h1>Criar Nova Despesa</h1>
             <form id="main" method="POST" action="{{ route('despesas.store') }}" enctype="multipart/form-data">
-    @endif
-
-    @csrf
-
-    <div class="form-group">
-        <label for="descricao">Descrição da Despesa</label>
-        <input type="text" name="descricao" id="descricao" class="form-control"
-            value="{{ old('descricao', optional($despesa)->descricao) }}" required>
-    </div>
-
-    <div class="form-group">
-        <label for="valor">Valor</label>
-        <input type="number" name="valor" id="valor" class="form-control" step="0.01"
-            value="{{ old('valor', optional($despesa)->valor) }}" required>
-    </div>
-
-    <div class="form-group">
-        <label for="data"> Data da Despesa</label>
-        <input type="date" name="data_despesa" id="data_despesa" class="form-control"
-            value="{{ old('data_despesa', optional($despesa)->data_despesa) }}" required>
-    </div>
-
-    <div class="form-group">
-        <label for="categoria">Categoria da Despesa</label>
-        <select name="categoria" id="categoria" class="form-control" required>
-            <option value="" selected>Selecione uma opção</option>
-            @foreach ($categorias as $categoria)
-                <option value="{{ $categoria['id'] }}"
-                    {{ optional($despesa)->categoria_id === $categoria->id ? 'selected' : '' }}>
-                    {{ $categoria->nome }}
-                </option>
-            @endforeach
-        </select>
-        <a href="{{ route('categorias.create') }}" class="btn btn-primary">Criar Nova Categoria</a>
-    </div>
-
-    <button type="submit" class="btn btn-primary">Salvar Despesa</button>
-
-    <a class='btn btn-secondary' href="{{ route('despesas.create') }}">
-        Cadastrar Nova Despesa
-    </a>
+                @endif
+                <div class="row">
+                    @csrf
+                    <div class="mb-3 col-12">
+                        <label for="descricao" class="form-label">Descrição da Despesa</label>
+                        <textarea class="form-control" name="descricao" id="descricao" required rows="3">{{ old('descricao', optional($despesa)->descricao) }}</textarea>
+                    </div>
 
 
-    </form>
+                    <div class="mb-3 col-12">
+                        <label for="categoria">Categoria da Despesa</label>
+                        <select name="categoria" id="categoria" class="form-control" required>
+                            <option value="" selected>Selecione uma opção</option>
+                            @foreach ($categorias as $categoria)
+                            <option value="{{ $categoria['id'] }}" {{ optional($despesa)->categoria_id === $categoria->id ? 'selected' : '' }}>
+                                {{ $categoria->nome }}
+                            </option>
+                            @endforeach
+                        </select>
+                    </div>
 
-    @if ($despesa && $despesa->id)
-        <form method="POST" action="{{ route('despesas.destroy', ['id' => $despesa->id]) }}">
-            @csrf
-            @method('DELETE')
-            <button type="submit" class="btn btn-danger">Deletar</button>
-        </form>
-    @endif
+                    <div class="mb-3 col-3">
+                        <label for="valor">Valor</label>
+                        <input type="number" name="valor" id="valor" class="form-control" step="0.01" value="{{ old('valor', optional($despesa)->valor) }}" required>
+                    </div>
 
-    <a class='btn btn-secondary' href="{{ route('despesas.index') }}">
-        Voltar
-    </a>
-@endsection
+                    <div class="mb-3 col-3">
+                        <label for="data"> Data da Despesa</label>
+                        <input type="date" name="data_despesa" id="data_despesa" class="form-control" value="{{ old('data_despesa', optional($despesa)->data_despesa) }}" required>
+                    </div>
+                </div>
+
+
+                <a href="{{ route('categorias.create') }}" class="btn btn-primary">Criar Nova Categoria</a>
+                <button type="submit" class="btn btn-primary">Salvar Despesa</button>
+
+                <a class='btn btn-secondary' href="{{ route('despesas.create') }}">
+                    Cadastrar Nova Despesa
+                </a>
+                <!-- @if ($despesa && $despesa->id) -->
+            </form>
+            <!-- <form method="POST" action="{{ route('despesas.destroy', ['id' => $despesa->id]) }}">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-danger">Deletar</button>
+            </form>
+            @endif
+     -->
+            <a class='btn btn-secondary' href="{{ route('despesas.index') }}">
+                Voltar
+            </a>
+            @endsection
+        </div>
--- routes/web.php
+++ routes/web.php
@@ -57,9 +57,9 @@ Route::get('/total-vendas', [VendaController::class, 'obterTotal']);
 Route::get('/despesas/{despesa}', [DespesasController::class, 'edit'])->name('despesas.edit');
 Route::get('/despesas/create', [DespesasController::class, 'create'])->name('despesas.create');
 Route::get('/despesas', [DespesasController::class, 'index'])->name('despesas.index');
-Route::delete('/despesas/{id}', [DespesasController::class, 'destroy'])->name('despesas.destroy');
 Route::put('/despesas/{id}', [DespesasController::class, 'update'])->name('despesas.update');
 Route::post('/despesas', [DespesasController::class, 'store'])->name('despesas.store');
+Route::delete('/despesas/{id}', [DespesasController::class, 'destroy'])->name('despesas.destroy');
 
 
 Route::get('/categorias/create', [CategoriasController::class, 'create'])->name('categorias.create');
