2562bfddd3a36e6df494897aae74bb94e9e3ac48
João Vitor <vitorribeiro9454@gmail.com>
2023-11-11 20:49:57 -0300

    deletar funcionando corretamente, ao deletar está apagando a fatura e retornando as placas como disponíveis

--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/receber-dados', [VendaController::class, 'ReceberDados']);
 Route::get('/compras-cliente', [VendaController::class, 'comprasCliente']);
 Route::get('/cliente/faturas', [VendaController::class, 'faturasCliente']);
+Route::delete('/vendas/{id}', [VendaController::class, 'deletarVenda'])->name('venda.deletar');
+
