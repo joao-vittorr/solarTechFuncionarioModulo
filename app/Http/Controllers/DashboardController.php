e43bd4ce3de18c26afbbe44654268b81b08934e5
João Vitor <vitorribeiro9454@gmail.com>
2023-11-22 16:14:49 -0300

    retornando os ultimos produtos cadastrados

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,6 +6,7 @@ use App\Models\Venda;
 use Illuminate\Support\Facades\View;
 use App\Models\Categorias;
 use Illuminate\Support\Facades\DB;
+use App\Models\Estoque;
 
 
 
@@ -29,26 +30,22 @@ class DashboardController extends Controller
             }
         }
 
+        // Lógica para obter as categorias mais vendidas
+        $categoriasMaisVendidas = Venda::select('nomePacote as categoria', DB::raw('count(*) as total'))
+        ->groupBy('nomePacote')
+        ->orderByDesc('total')
+        ->take(5) // Pegar as top 5 categorias mais vendidas
+        ->get();
+
+        // Buscar os últimos produtos cadastrados no estoque
+        // Obter os produtos mais recentes do estoque
+        $produtosRecentes = Estoque::orderBy('data_compra', 'desc')->take(5)->get();
+
+
         // Obter todos os meses disponíveis
         $mesesDisponiveis = array_keys($vendasPorMes);
 
-        // Obter as categorias mais vendidas
-        $categoriasMaisVendidas = $this->categoriasMaisVendidas();
-
         // Passe os dados para a view
-        return view('dashboard.index', compact('vendasPorMes', 'mesesDisponiveis', 'categoriasMaisVendidas'));
+        return view('dashboard.index', compact('vendasPorMes', 'mesesDisponiveis', 'produtosRecentes', 'categoriasMaisVendidas'));
     }
-
-    public function categoriasMaisVendidas()
-    {
-        // Lógica para obter as categorias mais vendidas
-        $categoriasMaisVendidas = Venda::select('nomePacote as categoria', DB::raw('count(*) as total'))
-            ->groupBy('nomePacote')
-            ->orderByDesc('total')
-            ->take(5) // Pegar as top 5 categorias mais vendidas
-            ->get();
-    
-        return $categoriasMaisVendidas;
-    }
-    
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -48,103 +48,28 @@
                 <div class="card">
                     <div class="card-header border-0">
                         <h3 class="card-title">Products</h3>
-                        <div class="card-tools">
-                            <a href="#" class="btn btn-tool btn-sm">
-                                <i class="fas fa-download"></i>
-                            </a>
-                            <a href="#" class="btn btn-tool btn-sm">
-                                <i class="fas fa-bars"></i>
-                            </a>
-                        </div>
                     </div>
                     <div class="card-body table-responsive p-0">
                         <table class="table table-striped table-valign-middle">
                             <thead>
                                 <tr>
-                                    <th>Product</th>
-                                    <th>Price</th>
-                                    <th>Sales</th>
-                                    <th>More</th>
+                                    <th>Produto</th>
+                                    <th>Valor</th>
+                                    <th>Data de Compra</th>
+                                    <th>Quantidade</th>
                                 </tr>
                             </thead>
                             <tbody>
+                                @foreach($produtosRecentes as $produto)
                                 <tr>
                                     <td>
-                                        <img src="dist/img/default-150x150.png" alt="Product 1" class="img-circle img-size-32 mr-2">
-                                        Some Product
+                                        {{ $produto->descricao }}
                                     </td>
-                                    <td>$13 USD</td>
-                                    <td>
-                                        <small class="text-success mr-1">
-                                            <i class="fas fa-arrow-up"></i>
-                                            12%
-                                        </small>
-                                        12,000 Sold
-                                    </td>
-                                    <td>
-                                        <a href="#" class="text-muted">
-                                            <i class="fas fa-search"></i>
-                                        </a>
-                                    </td>
+                                    <td>${{ $produto->valor }}</td>
+                                    <td>{{ $produto->data_compra}}</td>
+                                    <td>{{ $produto->quantidade }}</td>
                                 </tr>
-                                <tr>
-                                    <td>
-                                        <img src="dist/img/default-150x150.png" alt="Product 1" class="img-circle img-size-32 mr-2">
-                                        Another Product
-                                    </td>
-                                    <td>$29 USD</td>
-                                    <td>
-                                        <small class="text-warning mr-1">
-                                            <i class="fas fa-arrow-down"></i>
-                                            0.5%
-                                        </small>
-                                        123,234 Sold
-                                    </td>
-                                    <td>
-                                        <a href="#" class="text-muted">
-                                            <i class="fas fa-search"></i>
-                                        </a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <img src="dist/img/default-150x150.png" alt="Product 1" class="img-circle img-size-32 mr-2">
-                                        Amazing Product
-                                    </td>
-                                    <td>$1,230 USD</td>
-                                    <td>
-                                        <small class="text-danger mr-1">
-                                            <i class="fas fa-arrow-down"></i>
-                                            3%
-                                        </small>
-                                        198 Sold
-                                    </td>
-                                    <td>
-                                        <a href="#" class="text-muted">
-                                            <i class="fas fa-search"></i>
-                                        </a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <img src="dist/img/default-150x150.png" alt="Product 1" class="img-circle img-size-32 mr-2">
-                                        Perfect Item
-                                        <span class="badge bg-danger">NEW</span>
-                                    </td>
-                                    <td>$199 USD</td>
-                                    <td>
-                                        <small class="text-success mr-1">
-                                            <i class="fas fa-arrow-up"></i>
-                                            63%
-                                        </small>
-                                        87 Sold
-                                    </td>
-                                    <td>
-                                        <a href="#" class="text-muted">
-                                            <i class="fas fa-search"></i>
-                                        </a>
-                                    </td>
-                                </tr>
+                                @endforeach
                             </tbody>
                         </table>
                     </div>
