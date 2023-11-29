c2d0b65fed3848f3ff8d76b6291eb2cbe4fdbcf5
João Vitor <vitorribeiro9454@gmail.com>
2023-11-29 10:38:17 -0300

    melhorando a view de visitas tecnicas

--- resources/views/gerenciar-visitas/index.blade.php
+++ resources/views/gerenciar-visitas/index.blade.php
@@ -1,22 +1,33 @@
 @extends('layouts.template')
 
 @section('content')
+
 <div class="container">
-  <h1 class="mt-4">Usuários que solicitaram visita técnica</h1>
-  <p>Realize o gerenciamento de visitas técnicas.</p>
+
+  <div class="mt-4 align-items-center">
+    <h1 class="mt-4">Usuários que solicitaram visita técnica</h1>
+    <p>Realize o gerenciamento de visitas técnicas.</p>
+  </div>
 
   @foreach($usuariosComVisitas as $visita)
+  <br />
   <div class="row mb-3 text-center border border-2 rounded">
+    <div class="pb-1 col-md-6 themed-grid-col">
+      <p><strong>Detalhes: </strong>{{ $visita->detalhes }}</p>
+    </div>
     <div class="col-md-6 themed-grid-col">
-      <div class="pb-3">
-      <strong>Nome do Usuário: </strong>{{ $visita->user->name }}
-      </div>
       <div class="row">
-        <div class="col-md-6 themed-grid-col border-top border-2"><strong>CPF: </strong>{{ $visita->user->cpf }}</div>
-        <div class="col-md-6 themed-grid-col border-top border-start border-2"><strong>Data Agendada: </strong>{{ $visita->data_agendada }}</div>
+        <div class="p-1 col-md-12 themed-grid-col">
+          <strong>Nome do Usuário: </strong>{{ $visita->user->name }}
+        </div>
+        <div class="p-1 col-md-6 themed-grid-col">
+          <strong>CPF: </strong>{{ $visita->user->cpf }}
+        </div>
+        <div class="p-1 col-md-6 themed-grid-col">
+          <strong>Data Agendada: </strong>{{ $visita->data_agendada }}
+        </div>
       </div>
     </div>
-    <div class="col-md-6 themed-grid-col border-start border-2"><strong>Detalhes: </strong>{{ $visita->detalhes }}</div>
   </div>
   @endforeach
   <!-- Pagination Links -->
@@ -24,5 +35,4 @@
     {{ $usuariosComVisitas->links('components.pagination') }}
   </div>
 </div>
-
 @endsection
