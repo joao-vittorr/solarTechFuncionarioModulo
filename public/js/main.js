4f66e5ced21418f7adf3e190db3ae19eac9943bb
João Vitor <vitorribeiro9454@gmail.com>
2023-12-02 21:13:01 -0300

    Modificando modal de vendas

--- public/js/main.js
+++ public/js/main.js
@@ -59,97 +59,106 @@ $('.visualizar-pdf').on('click', function () {
     var dataCompra = $(this).data('compra-data')
 
     // Preencher os elementos HTML do modal com os dados
-    $('#myModal').find('.modal-body').html(`
-  <div class="modal-body row">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-12 col-md-offset-3 body-main">
-                <div class="col-md-12">
-                    <div class="row">
-                        <div class="col-md-4">
-                            <img class="img-fluid" alt="Logo" src="${window.location.origin}/images/logoProjetoSolarTech.png" />
-                        </div>
-                        <div class="col-md-8 text-end">
-                            <div>
-                                <h2><b>Fatura </b>#${vendaId}</h2>
+    $('#myModal').html(`
+    <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+        <div class="modal-header justify-content-end">
+            <button type="button" class="close btn btn-danger " data-dismiss="modal" aria-label="Fechar">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-body row">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12 col-md-offset-3 body-main">
+                        <div class="col-md-12">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <img class="img-fluid" alt="Logo" src="${window.location.origin}/images/logoProjetoSolarTech.png" />
+                                </div>
+                                <div class="col-md-8 text-end">
+                                    <div>
+                                        <h2><b>Fatura </b>#${vendaId}</h2>
+                                    </div>
+                                    <h4 style="color: #242424;"><strong>${nome}</strong></h4>
+                                    <h5 style="color: #242424;"><strong>CPF: ${cpf}</strong></h5>
+                                    <p>${logradouro}, ${numeroCasa}</p>
+                                    <p>${bairro},
+                                        ${cidade}/${estado}
+                                    </p>
+                                    <p>${email}</p>
+                                </div>
                             </div>
-                            <h4 style="color: #242424;"><strong>${nome}</strong></h4>
-                            <h5 style="color: #242424;"><strong>CPF: ${cpf}</strong></h5>
-                            <p>${logradouro}, ${numeroCasa}</p>
-                            <p>${bairro},
-                                ${cidade}/${estado}
-                            </p>
-                            <p>${email}</p>
+                            <br />
+                            <br />
+                            <div>
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th>
+                                                <h5>Descrição</h5>
+                                            </th>
+                                            <th>
+                                                <h5>Quantidade</h5>
+                                            </th>
+                                            <th>
+                                                <h5>Valor</h5>
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td class="col-md-6">${nomePacote}</td>
+                                            <td class="col-md-3">${quantidadePlacas}</td>
+                                            <td class="col-md-3">R$
+                                                ${valorFinal}
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th>Resumo: </th>
+                                            <td class="text-right">
+                                                <p>
+                                                    <strong>Subtotal: </strong>
+                                                </p>
+                                                <p>
+                                                    <strong>Valor Total: </strong>
+                                                </p>
+                                            </td>
+                                            <td>
+                                                <p>
+                                                    <strong>${quantidadePlacas}</strong>
+                                                </p>
+                                                <p>
+                                                    <strong>R$ ${valorFinal}
+                                                    </strong>
+                                                </p>
+                                            </td>
+                                        </tr>
+                                        <tr style="color: #272727;">
+                                            <td class="text-right">
+                                                <h4><strong>Total:</strong></h4>
+                                            </td>
+                                            <td></td>
+                                            <td class="text-left">
+                                                <h4><strong> R$
+                                                        ${valorFinal}</strong>
+                                                </h4>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div>
+                                <p><b>Data da Compra :</b> ${dataCompra} </p>
+                            </div>
                         </div>
-                    </div>
-                    <br />
-                    <br />
-                    <div>
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th>
-                                        <h5>Descrição</h5>
-                                    </th>
-                                    <th>
-                                        <h5>Quantidade</h5>
-                                    </th>
-                                    <th>
-                                        <h5>Valor</h5>
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td class="col-md-6">${nomePacote}</td>
-                                    <td class="col-md-3">${quantidadePlacas}</td>
-                                    <td class="col-md-3">R$
-                                        ${valorFinal}
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th>Resumo: </th>
-                                    <td class="text-right">
-                                        <p>
-                                            <strong>Subtotal: </strong>
-                                        </p>
-                                        <p>
-                                            <strong>Valor Total: </strong>
-                                        </p>
-                                    </td>
-                                    <td>
-                                        <p>
-                                            <strong>${quantidadePlacas}</strong>
-                                        </p>
-                                        <p>
-                                            <strong>R$ ${valorFinal}
-                                            </strong>
-                                        </p>
-                                    </td>
-                                </tr>
-                                <tr style="color: #272727;">
-                                    <td class="text-right">
-                                        <h4><strong>Total:</strong></h4>
-                                    </td>
-                                    <td></td>
-                                    <td class="text-left">
-                                        <h4><strong> R$
-                                                ${valorFinal}</strong>
-                                        </h4>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <div>
-                        <p><b>Data da Compra :</b> ${dataCompra} </p>
                     </div>
                 </div>
             </div>
-        </div>
+            </div>
+        <a href="#" id="gerarPdfLink" class="card-link btn btn-primary">Gerar PDF</a>
     </div>
-    </div>
-<a href="#" id="gerarPdfLink" class="card-link btn btn-primary">Gerar PDF</a>
+</div>
   `);
 
     $('#gerarPdfLink').on('click', function () {
